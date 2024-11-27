7c76075c701950c96ed1e6ed52a95a45553ad60d
Julian-Azange <julianazange@gmail.com>
2024-11-27 14:17:29 -0500

    MOD: se realizo el ajuste para mostrar la lista de vuelos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://punto-de-pago-air-frontend.vercel.app/",
 ]
 
 ROOT_URLCONF = "backend.urls"
